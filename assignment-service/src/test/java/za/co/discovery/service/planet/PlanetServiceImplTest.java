8b4d3e51bcd17df75565f20f79efe4c97ef145c3
tinashehondo <tinashehondo@gmail.com>
2017-04-19 06:29:07 +0200

    Unit tests resolved

--- assignment-service/src/test/java/za/co/discovery/service/planet/PlanetServiceImplTest.java
+++ assignment-service/src/test/java/za/co/discovery/service/planet/PlanetServiceImplTest.java
@@ -1,95 +1,95 @@
-//package za.co.discovery.service.planet;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import za.co.discovery.service.config.ServiceTestConfig;
-//import za.co.discovery.model.Planet;
-//
-//import static org.junit.Assert.assertEquals;
-//
-///**
-// * Created by tinashehondo on 4/16/17.
-// */
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = {ServiceTestConfig.class})
-//public class PlanetServiceImplTest {
-//
-//    @Autowired
-//    private PlanetService planetService;
-//
-//    @Test
-//    public void testSavePlanet() throws Exception {
-//
-//        Planet planet = new Planet();
-//        planet.setNode("A");
-//        planet.setName("Earth");
-//
-//        Planet savedPlanet = planetService.savePlanet(planet);
-//
-//        assert savedPlanet != null;
-//        assert savedPlanet.getNode() != null;
-//        assert savedPlanet.getName() != null;
-//
-//    }
-//
-//    @Test
-//    public void testFindPlanetById() throws Exception {
-//
-//        Planet planet = new Planet();
-//        planet.setNode("A");
-//        planet.setName("Earth");
-//        planetService.savePlanet(planet);
-//
-//        Planet searchedPlanet = planetService.findPlanetById("A");
-//
-//        assert searchedPlanet != null;
-//        assertEquals("Earth",searchedPlanet.getName());
-//        assert searchedPlanet.getNode() != null;
-//        assert searchedPlanet.getName() != null;
-//
-//    }
-//
-//    @Test
-//    public void testUpdatePlanet() throws Exception {
-//
-//        Planet planet = new Planet();
-//        planet.setNode("A");
-//        planet.setName("Earth");
-//        planetService.savePlanet(planet);
-//
-//        Planet planetTobeUpdated = new Planet();
-//        planetTobeUpdated.setNode("A");
-//        planetTobeUpdated.setName("Mars");
-//
-//        Planet updatedPlanet = planetService.savePlanet(planetTobeUpdated);
-//
-//        assert updatedPlanet != null;
-//        assertEquals("Mars",updatedPlanet.getName());
-//        assert updatedPlanet.getNode() != null;
-//        assert updatedPlanet.getName() != null;
-//
-//    }
-//
-//    @Test
-//    public void testDeletePlanet() throws Exception {
-//
-//        Planet planet = new Planet();
-//        planet.setNode("A");
-//        planet.setName("Earth");
-//        planetService.savePlanet(planet);
-//
-//        planetService.deletePlanet(planet);
-//
-//        Planet deletedPlanet = planetService.findPlanetById("A");
-//
-//        assertEquals(deletedPlanet,null);
-//
-//
-//    }
-//
-//}
-//
-//
+package za.co.discovery.service.planet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import za.co.discovery.service.config.ServiceTestConfig;
+import za.co.discovery.model.Planet;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by tinashehondo on 4/16/17.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ServiceTestConfig.class})
+public class PlanetServiceImplTest {
+
+    @Autowired
+    private PlanetService planetService;
+
+    @Test
+    public void testSavePlanet() throws Exception {
+
+        Planet planet = new Planet();
+        planet.setNode("A");
+        planet.setName("Earth");
+
+        Planet savedPlanet = planetService.savePlanet(planet);
+
+        assert savedPlanet != null;
+        assert savedPlanet.getNode() != null;
+        assert savedPlanet.getName() != null;
+
+    }
+
+    @Test
+    public void testFindPlanetById() throws Exception {
+
+        Planet planet = new Planet();
+        planet.setNode("A");
+        planet.setName("Earth");
+        planetService.savePlanet(planet);
+
+        Planet searchedPlanet = planetService.findPlanetById("A");
+
+        assert searchedPlanet != null;
+        assertEquals("Earth",searchedPlanet.getName());
+        assert searchedPlanet.getNode() != null;
+        assert searchedPlanet.getName() != null;
+
+    }
+
+    @Test
+    public void testUpdatePlanet() throws Exception {
+
+        Planet planet = new Planet();
+        planet.setNode("A");
+        planet.setName("Earth");
+        planetService.savePlanet(planet);
+
+        Planet planetTobeUpdated = new Planet();
+        planetTobeUpdated.setNode("A");
+        planetTobeUpdated.setName("Mars");
+
+        Planet updatedPlanet = planetService.savePlanet(planetTobeUpdated);
+
+        assert updatedPlanet != null;
+        assertEquals("Mars",updatedPlanet.getName());
+        assert updatedPlanet.getNode() != null;
+        assert updatedPlanet.getName() != null;
+
+    }
+
+    @Test
+    public void testDeletePlanet() throws Exception {
+
+        Planet planet = new Planet();
+        planet.setNode("A");
+        planet.setName("Earth");
+        planetService.savePlanet(planet);
+
+        planetService.deletePlanet(planet);
+
+        Planet deletedPlanet = planetService.findPlanetById("A");
+
+        assertEquals(deletedPlanet,null);
+
+
+    }
+
+}
+
+
--- assignment-service/src/test/java/za/co/discovery/service/route/RouteServiceImplTest.java
+++ assignment-service/src/test/java/za/co/discovery/service/route/RouteServiceImplTest.java
@@ -1,137 +1,168 @@
-//package za.co.discovery.service.route;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import za.co.discovery.model.Planet;
-//import za.co.discovery.model.Route;
-//import za.co.discovery.service.config.ServiceTestConfig;
-//import za.co.discovery.service.planet.PlanetService;
-//
-//import static org.junit.Assert.assertEquals;
-//
-///**
-// * Created by tinashehondo on 4/16/17.
-// */
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = {ServiceTestConfig.class})
-//public class RouteServiceImplTest {
-//
-//    @Autowired
-//    private RouteService routeService;
-//
-//    @Autowired
-//    private PlanetService planetService;
-//
-//
-//    @Test
-//    public void testSaveRoute() throws Exception {
-//
-//        Planet origin = new Planet();
-//        origin.setNode("A");
-//        origin.setName("EARTH");
-//
-//        planetService.savePlanet(origin);
-//
-//        Planet destination = new Planet();
-//        destination.setNode("B");
-//        destination.setName("MARS");
-//        planetService.savePlanet(destination);
-//
-//        Route route = new Route();
-//
-//        route.setRouteId(1);
-//        route.setOrigin(origin);
-//        route.setDestination(destination);
-//        route.setDistance(1.44);
-//
-//       Route savedRoute = routeService.saveRoute(route);
-//
-//
-//        assert savedRoute != null;
-//        assertEquals("A",savedRoute.getOrigin().getNode());
-//        assertEquals("B",savedRoute.getDestination().getNode());
-//
-//        assertEquals(1.44,savedRoute.getDistance(),0.01);
-//
-//    }
-//
-//    @Test
-//    public void testFindRouteById() throws Exception {
-//
-//        Planet origin = new Planet();
-//        origin.setNode("A");
-//        origin.setName("Earth");
-//
-//        planetService.savePlanet(origin);
-//
-//        Planet destination = new Planet();
-//        destination.setNode("B");
-//        destination.setName("Mars");
-//        planetService.savePlanet(destination);
-//
-//        Route route = new Route();
-//
-//        route.setRouteId(1);
-//        route.setOrigin(origin);
-//        route.setDestination(destination);
-//        route.setDistance(1.44);
-//
-//        routeService.saveRoute(route);
-//
-//        Route searchedRoute = routeService.findRouteById(1);
-//
-//        assert searchedRoute != null;
-//        assertEquals("Earth",searchedRoute.getOrigin().getName());
-//        assertEquals("Mars",searchedRoute.getDestination().getName());
-//    }
-//
-//   /* @Test
-//    public void testUpdateRoute() throws Exception {
-//
-//        Planet origin = new Planet();
-//        origin.setNode("A");
-//        origin.setName("EARTH");
-//
-//        planetService.savePlanet(origin);
-//
-//        Planet destination = new Planet();
-//        destination.setNode("B");
-//        destination.setName("MARS");
-//        planetService.savePlanet(destination);
-//
-//        Route route = new Route();
-//
-//        route.setRouteId(1);
-//        route.setOrigin(origin);
-//        route.setDestination(destination);
-//        route.setDistance(1.44);
-//
-//        routeService.saveRoute(route);
-//
-//        Planet newDestination = new Planet();
-//        newDestination.setNode("C");
-//        newDestination.setName("Moon");
-//        planetService.savePlanet(destination);
-//
-//        Route routeTobeUpdated = new Route();
-//
-//        route.setRouteId(1);
-//        routeTobeUpdated.setOrigin(origin);
-//        routeTobeUpdated.setDestination(newDestination);
-//        routeTobeUpdated.setDistance(2.67);
-//
-//        Route updatedRoute = routeService.saveRoute(routeTobeUpdated);
-//
-// assert updatedRoute != null;
-//        assertEquals("A",updatedRoute.getOrigin().getNode());
-//        assertEquals("C",updatedRoute.getDestination().getNode());
-//
-//        assertEquals(2.67,updatedRoute.getDistance(),0.01);
-//
-//    }
-//*/
-//
-//}
+package za.co.discovery.service.route;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import za.co.discovery.model.Planet;
+import za.co.discovery.model.Route;
+import za.co.discovery.service.config.ServiceTestConfig;
+import za.co.discovery.service.planet.PlanetService;
+
+import static org.junit.Assert.assertEquals;
+
+/*
+ * Created by tinashehondo on 4/16/17.
+
+*/
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ServiceTestConfig.class})
+public class RouteServiceImplTest {
+
+    @Autowired
+    private RouteService routeService;
+
+    @Autowired
+    private PlanetService planetService;
+
+
+    @Test
+    public void testSaveRoute() throws Exception {
+
+        Planet origin = new Planet();
+        origin.setNode("A");
+        origin.setName("EARTH");
+
+        planetService.savePlanet(origin);
+
+        Planet destination = new Planet();
+        destination.setNode("B");
+        destination.setName("MARS");
+        planetService.savePlanet(destination);
+
+        Route route = new Route();
+
+        route.setOrigin(origin);
+        route.setDestination(destination);
+        route.setDistance(1.44);
+
+       Route savedRoute = routeService.saveRoute(route);
+
+
+        assert savedRoute != null;
+        assertEquals("A",savedRoute.getOrigin().getNode());
+        assertEquals("B",savedRoute.getDestination().getNode());
+
+        assertEquals(1.44,savedRoute.getDistance(),0.01);
+
+    }
+
+    @Test
+    public void testFindRouteById() throws Exception {
+
+        Planet origin = new Planet();
+        origin.setNode("A");
+        origin.setName("Earth");
+
+        planetService.savePlanet(origin);
+
+        Planet destination = new Planet();
+        destination.setNode("B");
+        destination.setName("Mars");
+        planetService.savePlanet(destination);
+
+        Route route = new Route();
+
+        route.setRouteId(1);
+        route.setOrigin(origin);
+        route.setDestination(destination);
+        route.setDistance(1.44);
+
+        routeService.saveRoute(route);
+
+        Route searchedRoute = routeService.findRouteById(1);
+
+        assert searchedRoute != null;
+        assertEquals("Earth",searchedRoute.getOrigin().getName());
+        assertEquals("Mars",searchedRoute.getDestination().getName());
+    }
+
+ @Test
+    public void testUpdateRoute() throws Exception {
+
+        Planet origin = new Planet();
+        origin.setNode("A");
+        origin.setName("EARTH");
+
+        planetService.savePlanet(origin);
+
+        Planet destination = new Planet();
+        destination.setNode("B");
+        destination.setName("MARS");
+        planetService.savePlanet(destination);
+
+        Route route = new Route();
+
+        route.setRouteId(1);
+        route.setOrigin(origin);
+        route.setDestination(destination);
+        route.setDistance(1.44);
+
+        routeService.saveRoute(route);
+
+        Planet newDestination = new Planet();
+        newDestination.setNode("C");
+        newDestination.setName("Moon");
+        planetService.savePlanet(newDestination);
+
+        Route routeTobeUpdated = new Route();
+
+         routeTobeUpdated.setRouteId(1);
+        routeTobeUpdated.setOrigin(origin);
+        routeTobeUpdated.setDestination(newDestination);
+        routeTobeUpdated.setDistance(2.67);
+
+        Route updatedRoute = routeService.saveRoute(routeTobeUpdated);
+
+ assert updatedRoute != null;
+        assertEquals("A",updatedRoute.getOrigin().getNode());
+        assertEquals("C",updatedRoute.getDestination().getNode());
+
+        assertEquals(2.67,updatedRoute.getDistance(),0.01);
+
+    }
+
+    @Test
+    public void testDeleteRoute() throws Exception {
+
+        Planet origin = new Planet();
+        origin.setNode("X");
+        origin.setName("EARTH");
+
+        planetService.savePlanet(origin);
+
+        Planet destination = new Planet();
+        destination.setNode("B");
+        destination.setName("MARS");
+        planetService.savePlanet(destination);
+
+        Route route = new Route();
+
+        route.setRouteId(1);
+        route.setOrigin(origin);
+        route.setDestination(destination);
+        route.setDistance(1.44);
+
+        Route savedRoute = routeService.saveRoute(route);
+
+        routeService.deleteRoute(savedRoute);
+
+        Route deletedRoute = routeService.findRouteById(1);
+
+        assert deletedRoute == null;
+
+
+    }
+
+}
